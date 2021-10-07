c1397e69302e6590deefc249dff975284c69c9f3
Leadninja123 <92086301+Leadninja123@users.noreply.github.com>
2021-10-07 16:36:09 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,18 +1,17 @@
 const CONFIG = {
     titleWeb: "Làm ny em nha :3",
     introTitle: 'Lường Thị Linh Chi',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Trả lời thật lòng nha :)`,
     btnIntro: '^^HiHi^^',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 🥰',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'chị chọn đi 🥰',
+    desc: 'thật lòng nhé ',
     btnYes: 'Thích lắm <33',
     btnNo: 'Không nha :3',
     question: 'Trên thế giới hơn 7 tỉ người mà sao chị lại yêu em <3',
     btnReply: 'Gửi cho em <3',
     reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
     mess: 'em biết mà 🥰. Yêu chị nhiều nhiều 😘😘',
+    messDesc: 'chính thức nhá :3.',
+    btnAccept: 'okiiiiiiii <3',
     messLink: 'https://github.com/zukahai/Confess-Crush' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
